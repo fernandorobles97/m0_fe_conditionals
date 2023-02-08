5f4ec48b1f90f307fd7392784922de2105e12a18
Fernando Robles <fernandorobles97@yahoo.com>
2023-02-08 13:43:08 -0600

    Add answers for comparisons

--- comparisons.js
+++ comparisons.js
@@ -22,24 +22,31 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 
 // YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
 // this should log: true
+console.log("Is numberTeachers less than numberStudents?", numberTeachers < numberStudents);
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers?
 // this should log: false
+console.log("Is numberTeachers strictly equal to stringTeachers?", numberTeachers === stringTeachers);
 
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
 // this should log: true
+console.log("Is numberTeachers not equal to numberStudents?", numberTeachers !== numberStudents);
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
 // this should log: true
+console.log("Is numberStudents greater than or equal to 20?", numberStudents>= 20);
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
 // this should log: false
+console.log("Is numberStudents greater than or equal to 21?", numberStudents >= 21);
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
 // this should log: true
+console.log("Is numberStudents less than or equal to 20?", numberStudents <=20);
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
 // this should log: true
+console.log("Is numberStudents less than or equal to 21?", numberStudents <= 21);
 
 
 // #-------------------
@@ -52,22 +59,32 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 // Make sure YOU can explain it that way!
 
 console.log(4 < 9);
-// YOU DO: Explain.
+// YOU DO: Explain. 
+// The line of code is logging a conditional stating that the Number 4, using the comparison operator
+// symbol for is less than, the Number 9. The statement will evaluate to true because 4 is less than 9.
 
 var books = 3;
 console.log(4 < books);
 // YOU DO: Explain.
+// A variable is declared with the name of books and the Number 3.
+// A conditional is logged stating that Number 4, using the coparison operator for is less than, the name books. 
+// The statement will evaluate false, because 4 is not less than the books Number value 3.
 
 var friends = 6;
 var siblings = 2;
 console.log(friends > siblings);
-// YOU DO: Explain.
+// YOU DO: Explain. 
+// A variable is declared with the name of friends and the Number 6, another with the name of siblings
+// and the Number 2. A conditional is logged stating that the name friends is greater than the name siblings. 
+// This will evaluate to be true, because the Number 6 value for friends is greater than the Number 2 value for friends.
 
 var attendees = 9;
 var meals = 8;
 console.log(attendees !== meals);
 // YOU DO: Explain.
-
+//  A variable is declared with the name attendees and the Number value 9, another with the name meals and the Number value 8.
+// A conditional is logged stating that attendees is not strictly equal to meals. This will evaluate to be true because the attendees 
+// Number value does not equal the meals Number value.  
 
 // #-------------------
 // PART 3: Logical Operators
@@ -89,15 +106,17 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
-
+console.log(lovesToPlay && lovesTreats);
 
 // Determine if the dog loves to play and loves the dog park
-
+console.log(lovesToPlay && lovesDogPark);
 
 // Determine if the dog loves to play or loves the dog park
-
+console.log(lovesToPlay || lovesDogPark);
 
 // Determine if the dog loves to play and is a puppy
+console.log(lovesToPlay && age);
 
 // What did your final line of code evaluate to? Why do you think that is? Explain.
-// ANSWER:
+// ANSWER: It evaluted to 1. This is because there is no information regarding what age is considered a puppy.
+// We are simply given the Number value 1 for age, which when invoked in a conditional cannot evaluate true or false.
